7f28d149f3efbb82af0fb0f16282b3f256f0c577
Valentin Sanchez <elzero_2@hotmail.com>
2021-03-10 23:15:34 +0100

    Initial Commit

---
+++ LaLigApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaLigApp
+//
+//  Created by Valentín Sánchez Campañón on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
